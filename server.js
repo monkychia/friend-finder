287175d720d829718428c90f7b637d5aebfc1ea1
Catherine Chan <learnafew@gmail.com>
2018-08-15 17:14:12 -0700

    Initial check-in

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+var express = require("express");
+var mysql = require("mysql");
+
+// Create express app instance.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "", // moved to .env
+  database: "seinfeld_db"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
